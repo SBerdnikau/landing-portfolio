6ce883e0556991be58a58013a3fd8942e6f4b4ed
SBerdnikau <30774859+SBerdnikau@users.noreply.github.com>
2019-10-14 18:55:03 +0300

    update brakepoint 1

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import './scss/index.scss';
-import './images/';
+
+function importAll(r) {
+    let images = {};
+    r.keys().map((item, index) => { images[item.replace('./', '')] = r(item); });
+    return images;
+}
+const images = importAll(require.context('./images', false, /\.(png|jpe?g|svg)$/));
+console.log(images)
 
 
 
